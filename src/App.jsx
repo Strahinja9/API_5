244e0d0fbb4fd06d4acc0e1746a6fdd2288a208c
Strahinja <151401213+Strahinja9@users.noreply.github.com>
2025-05-16 00:58:07 +0200

    Changes to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,25 @@ import { useState } from 'react'
 import axios from 'axios'
 
 function App() {
-
+  // States
   const [movieName, setMovieName] = useState("");
+  const [movies, setMovies]= useState([]);
+  const [errors, setErrors]= useState();
+
+  // API consts
   const apiKey = import.meta.env.VITE_API_KEY;
+  const apiAdress = import.meta.env.VITE_OMDB_API;
+
+  // API
   const fetchMovieData = async (e) => {
     e.preventDefault(); 
   try {
-    const response = await axios.get(`http://www.omdbapi.com/?apikey=${apiKey}&t=${movieName}`);
-    console.log(response.data)
+    const response = await axios.get(`${apiAdress}?apikey=${apiKey}&s=${movieName}`);
+    setMovies(response.data.Search)
+    console.log(response.data.Search)
+    
   } catch (error) {
-    console.error('Error:', error);
+    setErrors('Error:', error);
     throw error;
   }
 };
@@ -25,6 +34,18 @@ function App() {
         <input type="text" onInput={ e => setMovieName(e.target.value)} value={movieName} />
         <button type='submit'>Pretrazi film</button>
       </form>
+      
+      {
+        movies.map((movie) => (
+            <ul key={movie.imdbID}>
+            <li>{movie.Title}</li>
+            <li>{movie.Actors}</li>
+            </ul>
+        ))
+      }
+
+      <p>{errors}</p>
+
     </>
   )
 }
